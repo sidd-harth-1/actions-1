20e1f731e6d39771655a48bc52670808e8e94a96
Siddharth <128633880+sidd-harth-1@users.noreply.github.com>
2023-09-21 19:43:10 +0530

    asd

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+
+const clusterAddress = process.env.MONGO_URI;
+const dbUser = process.env.MONGO_USERNAME;
+const dbPassword = process.env.MONGO_PASSWORD;
+
+
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     user: process.env.MONGO_USERNAME,
+//     pass: process.env.MONGO_PASSWORD,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }
